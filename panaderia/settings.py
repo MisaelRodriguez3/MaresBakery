0b6043eda0da4f46af1f62ff501ecc307a693143
Misael_Rodríguez <misaelrdz963@gmail.com>
2023-08-03 14:11:15 -0500

    eduardo

--- panaderia/settings.py
+++ panaderia/settings.py
@@ -148,7 +148,7 @@ WSGI_APPLICATION = "panaderia.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'MaresBakery_DB',
+        'NAME': 'maresBakery_db',
         'USER': 'maresbakery_db_user',
         'PASSWORD': 'ijnipPy0VyydO81wPhVHMuakVt3GVyt6',
         'HOST': 'dpg-cj5imjacn0vc73c6ms10-a.oregon-postgres.render.com',
