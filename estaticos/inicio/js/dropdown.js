b27156fa4fa2019a4a986d3ce7df0ff142e4dfc5
Misael_Rodríguez <misaelrdz963@gmail.com>
2023-08-03 11:36:19 -0500

    estaticos

---
+++ estaticos/inicio/js/dropdown.js
@@ -0,0 +1,12 @@
+document .querySelector(".usuario").addEventListener("click",function(){
+    document.querySelector(".submenuu").classList.toggle("show");
+})
+
+document .querySelector("#dropdown").addEventListener("click",function(){
+    document.querySelector("#dropdown-menu").classList.toggle("showw");
+})
+
+document .querySelector("#dropdown").addEventListener("click",function(){
+    document.querySelector(".top-show").classList.toggle("top");
+})
+
