5107f44b9c28b6af0bcfac0d43bcbf03302e8ac7
Misael_Rodríguez <misaelrdz963@gmail.com>
2023-08-03 16:35:39 -0500

    angell

--- panaderia/settings.py
+++ panaderia/settings.py
@@ -147,7 +147,7 @@ WSGI_APPLICATION = "panaderia.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(     
-        default='postgresql://maresfamily:Yng5cOSq33YobpWJaWmVWurfSEH9eFgW@dpg-cj62df4l975s73fk72o0-a/maresbakery_db_fnhd',      
+        default='postgresql://maresfamily:Yng5cOSq33YobpWJaWmVWurfSEH9eFgW@dpg-cj62df4l975s73fk72o0-a/maresbakery_db_fnhdJ',      
         conn_max_age=600    
         )}
 
