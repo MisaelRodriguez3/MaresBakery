8fedb5598d79ef0290afc34bed2039c0027197ac
Misael Rodríguez <135343507+MisaelRodriguez3@users.noreply.github.com>
2023-09-21 18:10:13 -0600

    Update admin.py

--- productos/admin.py
+++ productos/admin.py
@@ -23,7 +23,8 @@ class ProductosAdmin(admin.ModelAdmin):
 class OpinionesAdmin(admin.ModelAdmin):
     list_display = ('id', 'producto', 'opinion')
     search_fields = ('IDproducto__Nombre',)
-
+    ordering = ('IDopinion')
+S
 admin.site.register(Productos, ProductosAdmin)
 admin.site.register(Categorias, CategoriasAdmin)
 admin.site.register(Opiniones, OpinionesAdmin)
